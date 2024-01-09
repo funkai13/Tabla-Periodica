defaa34b70590fe36f2598e5ede3f9dc9418094b
funkai13 <evinssonmedrano@gmail.com>
2024-01-09 16:00:14 -0600

    main
    se dise;o la tabla

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import PeriodicTableData from './mocks/PeriodicTableData.json'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const colorMap = {
+  "noble gas": "#FFBC42",
+  "alkaline earth metal": "#EC674E",
+  "diatomic nonmetal": "#D81159",
+  "alkali metal": "#8F2D56",
+  "transition metal": "#58586B",
+  "post-transition metal": "#218380",
+  "lanthanide": "#4AABAF",
+  "metalloid": "#73D2DE",
+  "polyatomic nonmetal": "#909090",
+  "actinide":"#70abfa" 
+  
+};
+
+function handleClick(){
+  alert('que tranza carnal')}
+
+function App() {
+  const elements = PeriodicTableData.elements
+return(
+  <section className='h-screen flex items-center justify-center bg-slate-800'  >
+  <div className='grid  grid-cols-18  grid-rows-7 gap-1.5'>
+    {
+      elements.map(element=>(
+        <div onClick={handleClick} key={element.name} className='bg-clip-border border-2  cursor-pointer'  style={{
+          gridColumn: element.xpos,
+          gridRow: element.ypos,
+          borderColor: colorMap[element.category],
+          backgroundColor: colorMap[element.category],
+        }}>
+          <p className='text-white text-sm'>{element.number}</p>
+          <h1 className='text-white  text-center '>{element.symbol}</h1>
+          <h1 className='text-xs text-white text-center'>{element.name}</h1>
+        </div>
+   )) } 
+  </div>
+</section>
+)
+
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns:{
+        '18': 'repeat(18,minmax(0,1fr))',
+      } 
+    }
   },
   plugins: [],
 }
